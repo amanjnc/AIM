14253cc84a9b3b109a0aeb07a6ca16429174b4f9
amanuel <amanuelbeyne662@gmail.com>
2024-04-25 12:39:19 +0300

    data quality check

--- src/data_quality_check.py
+++ src/data_quality_check.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy import stats
 
 def checkEmpty(dataset):
 
@@ -14,23 +15,35 @@ def checkEmpty(dataset):
 data = pd.read_csv('data/benin-malanville.csv')        
 checkEmpty(data)
 
-# def checkOutliers(dataset):
     
-
-import pandas as pd
-
+    
 def checkIncorrectEntry(dataset):
     column_to_check = 'DHI'
 
     negative_values_count = (dataset[column_to_check] < 0).sum()
 
     if negative_values_count > 0:
-        print("Number of negative values in the column:", negative_values_count)
+        print("count -ve values in the column:", negative_values_count)
     else:
-        print("No negative values found in the column.")
+        print("No -ve values in the column.")
 
 
 data = pd.read_csv('data/benin-malanville.csv')        
 
 
 checkIncorrectEntry(data)
+
+
+# I used  z-scores for GHI column
+
+def checkOutlier(dataset):
+        
+    
+    data= dataset.drop('Comments',axis=1)
+    data= data.iloc[:,1:]
+    for column in data.columns:
+        z_scores = stats.zscore(data[column])
+        outliers = (z_scores > 3) | (z_scores < -3)
+        print(f"Outliers in {column} column:", outliers.sum())
+
+checkOutlier(data)
